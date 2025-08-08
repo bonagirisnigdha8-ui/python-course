cac54426dfb0a2401e7a120fbc758fdd5ac8f211
snigdha-130 <snigdhabonagiri8@gmail.co.uk>
2025-08-08 12:11:21 +0100

    initial commit

---
+++ lesson 2/activity.py
@@ -0,0 +1,13 @@
+print("welcome to the world of programming")
+
+#Print a number
+print(4)
+
+#next line after print
+print("Hello python /n")
+
+#pass multiple arguments for printing
+print("hello" ,5)
+
+#end argument of print statement
+print(" welcome to ", end= "*")
---
+++ lesson 2/activity2.py
@@ -0,0 +1,12 @@
+x = 5
+y  = "Jhon"
+print (x)
+print (y)
+
+coding = "codingal"
+print (coding)
+
+name = input ("enter your name :  ")
+
+print ("/nhello", name , "/nWelcome to codigal")
+
