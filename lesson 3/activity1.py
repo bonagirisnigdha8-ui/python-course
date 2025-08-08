ac96a191ad2562378304bf4d9839baef93f079e5
snigdha-130 <snigdhabonagiri8@gmail.co.uk>
2025-08-08 12:46:23 +0100

    initial commit

---
+++ lesson 3/activity1.py
@@ -0,0 +1,12 @@
+a = 5
+print ("type of a : ", type (a))
+
+b= 2.5 
+print ("type of b : ", type (b))
+
+c= "i am learning code"
+print ("type of b : ", type (c))
+
+d= True
+print ("type of b : ", type (d))
+
