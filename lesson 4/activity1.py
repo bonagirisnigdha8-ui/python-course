8710dd6d756664108b66b022e3cc780c69c52434
snigdha-130 <snigdhabonagiri8@gmail.co.uk>
2025-08-11 13:08:58 +0100

    initial commit

---
+++ lesson 4/activity1.py
@@ -0,0 +1,11 @@
+number1 = 7
+number2 = 2
+number3 = 4
+number4 = 6
+number5 = 5
+
+sum= number1+number2+number3+number4+number5
+print ("the sum of all the 5 numbers is :  ", sum)
+
+average = sum/5
+print ("the average of all the 5 numbers are :  " , average)
