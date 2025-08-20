d1cce10cbefce1aea31daa7110b564613bcfa9f3
snigdha-130 <snigdhabonagiri8@gmail.co.uk>
2025-08-20 11:44:21 +0100

    initial commit

--- activity2.py
+++ activity2.py
@@ -1,22 +1,11 @@
-a = 10
-b = 12
-c = 12
-
-print("a != b")
-print("b != c")
-
-a = "python"
-b = "coding"
-
-if a != b :
-    print(a, 'and', b, 'are different.')
-
-    a = 4
-    b = 5
-
-    if (a == 1) != (b == 5):
-        print('Hello')
-
-        a = int (input("enter a number  "))
-        if a%2 != 0  :
-            print(a, "is not an even number.")
+a= 5
+b= -10
+#print bitwise right shift operator
+print("a >> 1 =" , a << 1)
+#print ("b >> 1 =" , b >> 1)
+#a=5
+#b= -10
+# # print bitwise left shift operator
+print("a << 1 =" , a << 1)
+#print("a << 1 " , a << 1)
+#print("b << 1 =" , b << 1)
---
+++ lesson 6/activity1.py
@@ -0,0 +1,9 @@
+a= "Snigdha"
+b= "hello Samanvi"
+
+
+if(a in b):
+    print("true")
+
+if(a not in b):
+    print("false")
