e552b236264f0d4f1c84c55cac637462d2903c3e
snigdha-130 <snigdhabonagiri8@gmail.co.uk>
2025-08-22 12:31:36 +0100

    initial commit

--- activity2.py
+++ activity2.py
@@ -1,11 +1,14 @@
-a= 5
-b= -10
-#print bitwise right shift operator
-print("a >> 1 =" , a << 1)
-#print ("b >> 1 =" , b >> 1)
-#a=5
-#b= -10
-# # print bitwise left shift operator
-print("a << 1 =" , a << 1)
-#print("a << 1 " , a << 1)
-#print("b << 1 =" , b << 1)
+mean1=38
+wrong_number=36
+correct_number=56
+total_number=40
+
+#sum of 40 numbers
+sum=mean1*total_number
+print("The sum of 40 numbers:",sum)
+
+#correct sum of these numbers
+SUM2=sum-(wrong_number)+(correct_number)
+print("CORRECTED SUM",SUM2)
+
+#the correct mean
--- activity3.py
+++ activity3.py
@@ -1,28 +1,12 @@
-print("Enter Marks Obtained in 5 subjects")
-Maths = int(input())
-English = int(input())
-Science = int(input())
-History = int(input())
-Geography= int(input())
+s1 = int(input("enter your first speed: "))
+s2 = int(input("enter your second speed: "))
+s3 = int(input("enter your third speed: "))
 
-tot = Maths+English+Science+History+Geography
-avg = tot/5
+average = (s1 + s2 + s3 )/3
+print("the average speed is :  ",average)
 
-if avg>=91 and avg<100:
-    print("Your grade is A1")
-elif avg>81 and avg<91:
-    print("Your grade is A2")
-elif avg>71 and avg<81:
-    print("Your grade is B1")
-elif avg>61 and avg<71:
-    print("Your grade is B2")
-elif avg>51 and avg<61:
-    print("Your grade is C1")                
-elif avg>41 and avg<51:
-    print("Your grade is C2")  
-elif avg>33 and avg<41:
-    print("Your grade is D") 
-elif avg>21 and avg<33:
-    print("Your grade is E1") 
-elif avg>0 and avg<21:
-    print("Your grade is E2") 
+if s1<average:
+    print("Speed 1 is slower than average with thedifference of ", average-s1)
+
+    if s2<average:
+        print("Speed 2 is slower than average with thedifference of ", average-s2)
