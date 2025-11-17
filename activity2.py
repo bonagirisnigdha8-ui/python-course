f50077353b7d9af93e6318916b20525da67bc700
snigdha-130 <snigdhabonagiri8@gmail.co.uk>
2025-11-17 07:36:23 +0000

    initial commit

--- activity2.py
+++ activity2.py
@@ -1,26 +1,12 @@
+def match_words(words): 
+    ctr = 0
+    lst = []
+    for word in words:
+        if len(word) > 1 and word[0] == word[-1]:
+            ctr += 1
+            lst.append(word)
 
-def hotel_cost(nights):
-    return 140* nights
-
-#define a function called plane_ride_cost thet takes a string, city as input.
-def plane_ride_cost (city):
-    if "Charlotte"== city:
-        return 183
-    elif "Tampa"== city:
-        return 220 
-    elif "Pittsburgh"== city:
-        return 222 
-    elif "Los Angels"== city:
-        return 475 
-    
-#define a functiom called rental_car_cost with an arguement called days
-def rental_car_cost(days):
-    if days>=7 :
-        return 40*days - 50
-    elif days>=3 :
-        return 40*days - 20
-    else :
-        return 40*days
-
-#define a function called trip cost with arguement day,money and city
-def trip_cost 
+            print("List of words with first and last character same", lst)
+            return ctr
+        count = match_words(['abc', 'cfc', 'xyz' ,'aba' ,'1221' ])
+        print("number of words having first and last character same:",)
