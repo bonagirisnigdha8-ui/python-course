77808883a46634f08ae6a12ddab8988b4c4d064f
snigdha-130 <snigdhabonagiri8@gmail.co.uk>
2025-12-29 16:35:36 +0000

    initial commit

---
+++ Lesson 22/activity1.py
@@ -0,0 +1,15 @@
+
+my_set = {1, 2, 3}
+print(my_set)
+my_set = {1.0, "hello", (1, 2, 3)}
+print(my_set)
+my_set = {1, 2, 3, 4, 3, 2}
+print(my_set)
+my_set = {[1, 2, 3, 2]}
+print(my_set,"/n")
+num_set = set([0, 1, 2, 3, 4, 5])
+print("Origial set: ")
+print(num_set)
+num_set.pop()
+print("After removing the first element fron the set said: ")
+print(my_set,"/n")
--- activity2.py
+++ activity2.py
@@ -1,18 +1,8 @@
-#function to check whether palindrome or not
-def palind(r):
-    e = len(r) -1
-    s = 0
-    while(s<e):
-        if(r[s]!=r[e]):
-            return False
-        s+=1
-        e-=1
-    return True
-
-
-r = (1,2,3,3,2,1)
-
-if (palind(r)):
-    print("The tuple is Flip-flop")
-else:
-    print("The tuple is not Flip-flop")
+setx = {"green", "blue"}
+sety = {"green", "yellow"}
+print("Original set elements: ")
+print(setx)
+print(sety)
+print("Intersection of 2 said sets: ")
+setz = setx.intersection(sety)
+print (setz)
