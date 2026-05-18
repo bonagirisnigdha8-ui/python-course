0513f2f66986a822427f6c1973dcd8f4f6089f13
snigdha-130 <snigdhabonagiri8@gmail.co.uk>
2026-05-18 16:16:32 +0100

    initial commit

---
+++ lesson 34/activity1.py
@@ -0,0 +1,12 @@
+# Import necessary libraries
+from tkinter import *
+
+# Create window
+window = Tk()
+
+# Set the window Title and Geometry
+window.title('Demo Window')
+window.geometry('400x300')
+
+#Start the Gui event loop
+window.mainloop
